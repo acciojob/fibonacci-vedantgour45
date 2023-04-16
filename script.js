18aad08593dc8a05677dcb6e44f27009281de62a
Vedant Gour <113295244+vedantgour45@users.noreply.github.com>
2023-04-16 11:49:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function fibonacci(num) {
 // your code here
 	int fib = [0, 1];
 
-	return fibonacci(fib[0]-1)+fibonacci(fib[1]-1);
+	return fibonacci(fib[0]-1)+fibonacci(fib[1]-2);
 }
 
 module.exports = fibonacci;
