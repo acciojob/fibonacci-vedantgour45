04e2a783e470eefaf1443aab1d5c90c6db552270
Vedant Gour <113295244+vedantgour45@users.noreply.github.com>
2023-04-25 19:02:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function fibonacci(num) {
-  if (num === 0 || num === 1) {
-    return num;
-  } else {
-    return fibonacci(num - 1) + fibonacci(num - 2);
-  }
+// your code here
+	if(num <=1) return num;
+	let fib = [0,1];
+	for(let i = 2;i<=num;i++){
+      fib[i] = fib[i-1]+fib[i-2];
 }
-
+	return fib[num];
+}
 
 module.exports = fibonacci;
